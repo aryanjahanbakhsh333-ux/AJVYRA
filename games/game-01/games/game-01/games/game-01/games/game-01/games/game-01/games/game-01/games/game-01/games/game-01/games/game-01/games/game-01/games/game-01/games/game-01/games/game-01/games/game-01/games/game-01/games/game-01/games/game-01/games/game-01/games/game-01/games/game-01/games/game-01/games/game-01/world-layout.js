a005d65c42b7d73018b9236861673ba8a493958e
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-17 18:54:04 +0300

    Create world-layout.js

---
+++ games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/world-layout.js
@@ -0,0 +1,66 @@
+const WORLD_LAYOUT = [
+
+    {
+        x: 1200,
+        y: 670,
+        width: 2400,
+        height: 60,
+        color: 0x181818
+    },
+
+    {
+        x: 180,
+        y: 520,
+        width: 260,
+        height: 30,
+        color: 0x292929
+    },
+
+    {
+        x: 560,
+        y: 440,
+        width: 220,
+        height: 30,
+        color: 0x292929
+    },
+
+    {
+        x: 900,
+        y: 350,
+        width: 240,
+        height: 30,
+        color: 0x292929
+    },
+
+    {
+        x: 1250,
+        y: 470,
+        width: 280,
+        height: 30,
+        color: 0x292929
+    },
+
+    {
+        x: 1600,
+        y: 380,
+        width: 220,
+        height: 30,
+        color: 0x292929
+    },
+
+    {
+        x: 1940,
+        y: 300,
+        width: 250,
+        height: 30,
+        color: 0x292929
+    },
+
+    {
+        x: 2220,
+        y: 440,
+        width: 220,
+        height: 30,
+        color: 0x292929
+    }
+];

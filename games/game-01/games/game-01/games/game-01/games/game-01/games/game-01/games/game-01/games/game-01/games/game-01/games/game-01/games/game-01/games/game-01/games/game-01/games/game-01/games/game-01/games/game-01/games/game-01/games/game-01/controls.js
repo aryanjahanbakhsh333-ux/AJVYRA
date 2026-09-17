b769c0244872a80c8fd5303b00bc064d550c995c
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-17 18:51:20 +0300

    Create controls.js

---
+++ games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/controls.js
@@ -0,0 +1,65 @@
+function setupControls(scene) {
+
+    const keyboard =
+        scene.input.keyboard;
+
+
+    keyboard.on(
+        "keydown-LEFT",
+        () => {
+
+            CONTROL_STATE.left = true;
+
+        }
+    );
+
+
+    keyboard.on(
+        "keyup-LEFT",
+        () => {
+
+            CONTROL_STATE.left = false;
+
+        }
+    );
+
+
+    keyboard.on(
+        "keydown-RIGHT",
+        () => {
+
+            CONTROL_STATE.right = true;
+
+        }
+    );
+
+
+    keyboard.on(
+        "keyup-RIGHT",
+        () => {
+
+            CONTROL_STATE.right = false;
+
+        }
+    );
+
+
+    keyboard.on(
+        "keydown-UP",
+        () => {
+
+            CONTROL_STATE.jump = true;
+
+        }
+    );
+
+
+    keyboard.on(
+        "keyup-UP",
+        () => {
+
+            CONTROL_STATE.jump = false;
+
+        }
+    );
+}

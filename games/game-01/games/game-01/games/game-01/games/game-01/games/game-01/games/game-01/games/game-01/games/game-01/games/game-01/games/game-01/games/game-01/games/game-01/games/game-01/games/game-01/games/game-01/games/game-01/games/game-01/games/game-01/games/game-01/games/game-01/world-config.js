046b1dc0839d86d4cc852110d4ec4a0bc117a657
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-17 18:53:15 +0300

    Create world-config.js

---
+++ games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/world-config.js
@@ -0,0 +1,14 @@
+const WORLD_CONFIG = {
+    width: 2400,
+    height: 700,
+
+    background: 0x050505,
+
+    groundHeight: 60,
+
+    fogEnabled: true,
+
+    fogColor: 0x050505,
+
+    fogAlpha: 0.35
+};

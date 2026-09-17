6a73cc938edd0f7405f6cc0ad6e4e06d5dccd231
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-17 18:40:25 +0300

    Create asset-loader.js

---
+++ games/game-01/games/game-01/asset-loader.js
@@ -0,0 +1,28 @@
+function loadGameAssets(scene) {
+
+    /*
+     * وقتی Assetهای اصلی AJVYRA آماده شدند،
+     * مسیر آنها را اینجا اضافه می‌کنیم.
+     */
+
+    // Example:
+    //
+    // scene.load.image(
+    //     "player",
+    //     "assets/player.png"
+    // );
+    //
+    // scene.load.spritesheet(
+    //     "player-run",
+    //     "assets/player-run.png",
+    //     {
+    //         frameWidth: 64,
+    //         frameHeight: 64
+    //     }
+    // );
+    //
+    // scene.load.audio(
+    //     "coin",
+    //     "assets/coin.mp3"
+    // );
+}

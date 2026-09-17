e615507da525518d04d305fe15592dccc5f7cfe4
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-17 18:43:51 +0300

    Create level-data.js

---
+++ games/game-01/level-data.js
@@ -0,0 +1,30 @@
+const LEVEL_DATA = {
+
+    1: {
+        name: "The First Shadow",
+
+        width: 1800,
+
+        startingLives: 3,
+
+        timeLimit: 90,
+
+        coins: 5,
+
+        enemies: 3
+    },
+
+    2: {
+        name: "Coming Soon",
+
+        width: 2200,
+
+        startingLives: 3,
+
+        timeLimit: 100,
+
+        coins: 7,
+
+        enemies: 5
+    }
+};

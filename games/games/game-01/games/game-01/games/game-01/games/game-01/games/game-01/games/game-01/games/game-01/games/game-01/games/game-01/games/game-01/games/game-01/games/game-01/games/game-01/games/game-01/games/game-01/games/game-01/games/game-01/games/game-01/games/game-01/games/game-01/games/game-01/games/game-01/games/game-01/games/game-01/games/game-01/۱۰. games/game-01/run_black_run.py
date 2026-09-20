8c08934463a58f87089318d641350bb7397dcacc
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-20 05:42:36 +0300

    Create run_black_run.py

---
+++ games/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/games/game-01/۱۰. games/game-01/run_black_run.py
@@ -0,0 +1,16 @@
+from python_boot import boot
+
+boot()
+
+from black_run_main import BlackRun
+
+
+def main():
+
+    game = BlackRun()
+
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
